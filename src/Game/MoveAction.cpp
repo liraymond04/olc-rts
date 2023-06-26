66859a806e2ffbd983f25266ae2e745bec1cf9d5
liraymond04 <li.raymond04@gmail.com>
2023-06-25 23:08:18 -0600

    Fix obstacles during move action

--- src/Game/MoveAction.cpp
+++ src/Game/MoveAction.cpp
@@ -15,6 +15,17 @@ MoveAction::MoveAction(float targetTime, HexGrid *hexGrid, Unit *unit,
 
 void MoveAction::Tick() {
     Hex cur = unit->path[unit->i];
+    if (hexGrid->_units.at(cur.q, cur.r) != nullptr) {
+        Hex newPos = unit->path.back();
+        if (newPos == Hex(cur.q, cur.r)) {
+            done = true;
+            return;
+        }
+        unit->path.clear();
+        unit->i = 0;
+        hexGrid->A_Star(unit->path, &unit->pos, &newPos);
+        cur = unit->path[unit->i];
+    }
     hexGrid->_units.at(unit->pos.q, unit->pos.r) = nullptr;
     hexGrid->_units.at(cur.q, cur.r) = unit;
     unit->pos = cur;
